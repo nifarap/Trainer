e807d642c8453b6bb17bd86134ee2f7a1792c7f0
nifarap <vo1k@list.ru>
2018-05-24 10:42:06 +0300

    23.05

--- src/main/java/com/getjavajob/training/algo/init/fateevs/TaskCh02N031.java
+++ src/main/java/com/getjavajob/training/algo/init/fateevs/TaskCh02N031.java
@@ -1,17 +1,25 @@
 package com.getjavajob.training.algo.init.fateevs;
 
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class TaskCh02N031 {
     public static void main (String args[]){
-        int n;
-        int x;
+        int x = 0;
 
         System.out.print("Введите число от 100 до 999: ");
         Scanner in = new Scanner(System.in);
-        System.out.println("Вы ввели число " + in.nextInt());
+        x = in.nextInt();
+        System.out.println("Вы ввели число " + x);
 
+        int a, b, c, y;
 
+        a = x % 10;
+        b = (x - a)/10 % 10;
+        c = (x - (x % 100)) / 100;
 
+        y = c*100 + a*10 + b;
+
+        System.out.println("Изначальное число = " +y);
     }
 }
---
+++ src/main/java/com/getjavajob/training/algo/init/fateevs/TaskCh02N039.java
@@ -0,0 +1,7 @@
+package com.getjavajob.training.algo.init.fateevs;
+
+public class TaskCh02N039 {
+
+    
+
+}
